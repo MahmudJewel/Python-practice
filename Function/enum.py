0fbe03ee1c88671668b6ee3516edb42590fd2761
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2023-03-27 16:21:50 +0600

    enum added on python

---
+++ Function/enum.py
@@ -0,0 +1,10 @@
+# #structure-1
+# l1 = ["eat", "sleep", "repeat"]
+# s1 = "mahmud"
+# print(list(enumerate(l1)))
+# print(list(enumerate(s1)))
+
+# ex-2
+lst = ['zero', 'one', 'two', 'three', 'four']
+for i, j in enumerate(lst):
+    print(i, j)
